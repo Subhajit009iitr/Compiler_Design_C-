4ce1434d7fcef2082c933f77bc0839f6485e8c0c
Subhajit009iitr <biswassubhajit009@gmail.com>
2024-03-23 13:52:02 +0530

    Initialise git

---
+++ input.cs
@@ -0,0 +1,11 @@
+using System; class Program {     
+    static void Main(string[] args)     {
+        int x = 10;
+        int y = 20;
+        int sum = AddNumbers(x, y);
+        Console.WriteLine("Sum: " + sum);
+        }  
+        static int AddNumbers(int a, int b)     {         
+        return a + b;     
+    } 
+} 
